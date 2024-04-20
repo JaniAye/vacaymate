551293fc1e6c88d682503de20e7d07b0801428f3
Janidu Ayeshan <janiduayeshan16@gmail.com>
2024-04-20 11:09:14 +0530

    add notification icon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -32,6 +32,7 @@
 
     <link href="css/style.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/index.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/notification.css') }}">
 
     <!-- Slick Carousel CSS -->
     <link rel="stylesheet" type="text/css"
@@ -50,37 +51,6 @@
         </div>
     </div>
     <!-- Spinner End -->
-
-
-    <!-- Topbar Start -->
-    <div class="container-fluid bg-dark px-5 d-none d-lg-block">
-        <div class="row gx-0">
-            <div class="col-lg-8 text-center text-lg-start mb-2 mb-lg-0">
-                <div class="d-inline-flex align-items-center" style="height: 45px;">
-                    <small class="me-3 text-light"><i class="fa fa-map-marker-alt me-2"></i>123 Street, New York,
-                        USA</small>
-                    <small class="me-3 text-light"><i class="fa fa-phone-alt me-2"></i>+012 345 6789</small>
-                    <small class="text-light"><i class="fa fa-envelope-open me-2"></i>info@example.com</small>
-                </div>
-            </div>
-            <div class="col-lg-4 text-center text-lg-end">
-                <div class="d-inline-flex align-items-center" style="height: 45px;">
-                    <a class="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2" href=""><i
-                            class="fab fa-twitter fw-normal"></i></a>
-                    <a class="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2" href=""><i
-                            class="fab fa-facebook-f fw-normal"></i></a>
-                    <a class="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2" href=""><i
-                            class="fab fa-linkedin-in fw-normal"></i></a>
-                    <a class="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2" href=""><i
-                            class="fab fa-instagram fw-normal"></i></a>
-                    <a class="btn btn-sm btn-outline-light btn-sm-square rounded-circle" href=""><i
-                            class="fab fa-youtube fw-normal"></i></a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Topbar End -->
-
 
     <!-- Navbar & Hero Start -->
     <div class="container-fluid position-relative p-0">
@@ -110,6 +80,29 @@
                     </div>
                     <a href="contact.html" class="nav-item nav-link">Contact</a>
                 </div>
+
+                {{-- notifications --}}
+                <div class="notification">
+                    <input type="checkbox" name="uro" id="uro">
+                    <input type="checkbox" name="btn" id="btn">
+                    <label for="btn">
+                        <span class="counter">2</span>
+                    </label>
+                    <div class="panel">
+                        <div class="unread-only">Only show unread <label for="uro"></label></div>
+                        <ul>
+                            <li class="unread"><img src="https://i.pravatar.cc/40?img=8"><strong>Tene</strong> added
+                                your Pen <a href="#">404 Error Room</a> to their Collection</li>
+                            {{-- <li class="unread"><img src="https://i.pravatar.cc/40?img=11"><strong>Sem</strong> and 14
+                                others loves your pen <a href="#">Crack Climbing Map</a></li>
+                            <li class="read"><img src="https://i.pravatar.cc/40?img=3"><strong>Rufo</strong> and 11
+                                others loves your pen <a href="#">Notification Icon</a></li>
+                            <li class="read"><img src="https://i.pravatar.cc/40?img=4"><strong>Wirki</strong> and 6
+                                others followed you</li> --}}
+                        </ul>
+                    </div>
+                </div>
+                {{-- notifications end --}}
                 <a href="" class="btn btn-primary rounded-pill py-2 px-4">User Portal</a>
             </div>
         </nav>
