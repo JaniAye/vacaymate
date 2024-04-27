8a002d9ae37975761e57f313a5520f290597e9f0
Janidu Ayeshan <janiduayeshan16@gmail.com>
2024-04-28 01:19:57 +0530

    hotel tab remove

--- resources/views/add-package.blade.php
+++ resources/views/add-package.blade.php
@@ -2601,6 +2601,40 @@
             }
         }
 
+        function removeHotelCard(event) {
+            var target = event.target;
+            var parentDiv = target.closest('.col-lg-3.col-md-6.wow.fadeInUp');
+
+            if (parentDiv) {
+                parentDiv.remove();
+
+
+                var hotelList = document.getElementById("accommodationList");
+
+                if (hotelList) {
+
+                    var hotelElements = hotelList.querySelectorAll(".col-lg-3.col-md-6.wow.fadeInUp");
+
+                    for (let index = 0; index < hotelElements.length; index++) {
+                        var currentElement = hotelElements[index];
+                        var currentVehiClass = Array.from(currentElement.classList).find(cls => cls.startsWith('hotel'));
+
+                        if (currentVehiClass) {
+                            currentElement.classList.remove(currentVehiClass);
+                        }
+
+
+                        currentElement.classList.add(`hotel${index + 1}`);
+                    }
+                }
+
+            } else {
+                console.warn("Parent div not found for removal.");
+            }
+        }
+
+
+
         function addHotels(event) {
 
             const cnt = localStorage.getItem('cnt');
@@ -2698,7 +2732,7 @@
                                     <small class="fa fa-star text-primary"></small>
                                 </div>
                                 <p>${data[0].discription}</p>
-                                <button class="rmv-btn " style=" margin-left: 40%;">
+                                <button class="rmv-btn " style=" margin-left: 40%;" onclick='removeHotelCard(event)'>
                                     <svg viewBox="0 0 448 512" class="svgIcon">
                                         <path
                                             d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z">
