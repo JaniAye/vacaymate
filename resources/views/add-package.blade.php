40a85b37f69a2c964c27755e6d7e8a3b5d70a6e9
Janidu Ayeshan <janiduayeshan16@gmail.com>
2024-04-27 23:00:20 +0530

    delete button issue fixed

--- resources/views/add-package.blade.php
+++ resources/views/add-package.blade.php
@@ -2418,6 +2418,7 @@
         }
 
 
+
         //vehicle search click result set
         function addToVehicleList(event) {
             const cnt = localStorage.getItem('cnt');
@@ -2508,7 +2509,7 @@
                                     <small class="fa fa-star text-primary"></small>
                                 </div>
                                 <p>${data[0].discription}</p>
-                                <button class="rmv-btn " style=" margin-left: 40%;">
+                                <button class="rmv-btn " style=" margin-left: 40%;" onclick='removeVehicleCard(event)'>
                                     <svg viewBox="0 0 448 512" class="svgIcon">
                                         <path
                                             d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z">
@@ -2554,6 +2555,39 @@
 
 
         }
+
+        function removeVehicleCard(event) {
+            var target = event.target;
+            var parentDiv = target.closest('.col-lg-3.col-md-6.wow.fadeInUp');
+
+            if (parentDiv) {
+                parentDiv.remove();
+
+
+                var VehicleList = document.getElementById("VehicleList");
+
+                if (VehicleList) {
+
+                    var vehiElements = VehicleList.querySelectorAll(".col-lg-3.col-md-6.wow.fadeInUp");
+
+                    for (let index = 0; index < vehiElements.length; index++) {
+                        var currentElement = vehiElements[index];
+                        var currentVehiClass = Array.from(currentElement.classList).find(cls => cls.startsWith('vehi'));
+
+                        if (currentVehiClass) {
+                            currentElement.classList.remove(currentVehiClass);
+                        }
+
+
+                        currentElement.classList.add(`vehi${index + 1}`);
+                    }
+                }
+
+            } else {
+                console.warn("Parent div not found for removal.");
+            }
+        }
+
         // date counter
         function incrementDt() {
 
