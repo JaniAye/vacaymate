9968d1de3cb1b8a8c84912eacc95a5582940cb2a
Janidu Ayeshan <janiduayeshan16@gmail.com>
2024-04-27 22:38:45 +0530

    delete button function

--- resources/views/add-package.blade.php
+++ resources/views/add-package.blade.php
@@ -2292,6 +2292,7 @@
                                     '.col-lg-2.col-md-6.wow.fadeInUp');
                                 var columnCount = columnElements.length;
 
+
                                 // if (columnElementsff === null) {} else {
                                 for (let index = 0; index < columnCount; index++) {
                                     // if (columnCount < 6) {
@@ -2301,7 +2302,6 @@
                                     content += columnElementsff.outerHTML;
                                     // }
                                 }
-
                                 // }
 
                                 var packageListuda = document.getElementById('packageList');
@@ -2344,7 +2344,7 @@
                                     <small class="fa fa-star text-primary"></small>
                                 </div>
                                 <p>${data[0].discription}</p>
-                                <button class="rmv-btn " style=" margin-left: 30%;">
+                                <button class="rmv-btn " style=" margin-left: 30%;" onclick='removeLocCard(event)'>
                                     <svg viewBox="0 0 448 512" class="svgIcon">
                                         <path
                                             d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z">
@@ -2381,6 +2381,40 @@
 
 
 
+        }
+
+        function removeLocCard(event) {
+            var target = event.target;
+            var parentDiv = target.closest('.col-lg-2.col-md-6.wow.fadeInUp');
+
+            if (parentDiv) {
+                parentDiv.remove(); // Remove the element
+
+                // After removing, adjust the class names to maintain sequential order
+                var packageList = document.getElementById("packageList");
+
+                if (packageList) {
+                    // Find all the remaining elements with loc class
+                    var locElements = packageList.querySelectorAll(".col-lg-2.col-md-6.wow.fadeInUp");
+
+                    // Loop through all remaining elements to reset their loc class
+                    for (let index = 0; index < locElements.length; index++) {
+                        // Find the current loc class and remove it
+                        var currentElement = locElements[index];
+                        var currentLocClass = Array.from(currentElement.classList).find(cls => cls.startsWith('loc'));
+
+                        if (currentLocClass) {
+                            currentElement.classList.remove(currentLocClass); // Remove the existing loc class
+                        }
+
+                        // Assign the new loc class based on the current index
+                        currentElement.classList.add(`loc${index + 1}`); // Reset the loc classes
+                    }
+                }
+
+            } else {
+                console.warn("Parent div not found for removal.");
+            }
         }
 
 
